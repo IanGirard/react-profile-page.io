11421b7aa69c37c70ec453bb337658a0a9c96319
IanGirard <iankeinert@gmail.com>
2018-01-17 09:05:21 -0700

    2nd commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import screenShotOne from '../../assets/project-images/screenShotOne.png';
+import screenShotTwo from '../../assets/project-images/screenShotTwo.png';
+import screenShotThree from '../../assets/project-images/screenShotThree.png';
 
 class Projects extends Component {
     render() {
@@ -11,16 +14,16 @@ class Projects extends Component {
                     <div><Navbar/></div>
                     <div className="projects-main-container">
                       <div className="project-row-1">
-                        <div className="row-1-left"></div>
-                        <div className="row-1-right"></div>
+                        <div className="row-1-left"><img src={screenShotThree} alt="screen-shot-three"/></div>
+                        <div className="row-1-right"><h1>Title 1 Here</h1><p>Some text about the first project</p></div>
                       </div>
                       <div className="project-row-2">
-                        <div className="row-2-left"></div>
-                        <div className="row-2-right"></div>
+                        <div className="row-2-left"><h1>Title 2 Here</h1><p>Some text about the second project</p></div>
+                        <div className="row-2-right"><img src={screenShotOne} alt="screen-shot-one"/></div>
                       </div>
                       <div className="project-row-3">
-                        <div className="row-3-left"></div>
-                        <div className="row-3-right"></div>
+                        <div className="row-3-left"><img src={screenShotTwo} alt="screen-shot-two"/></div>
+                        <div className="row-3-right"><h1>Title 3 Here</h1><p>Some text about the first project</p></div>
                       </div>
                       </div>
 
