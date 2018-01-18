7727512341500022904ff99379a8a92de7364d2c
IanGirard <iankeinert@gmail.com>
2018-01-18 11:35:41 -0700

    trying BrowserRouter

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import { Route, HashRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Contact from './components/Contact/Contact';
 import Projects from './components/Projects/Projects';
 
 
 export default (
-    <HashRouter>
+    <BrowserRouter>
         <div>
             <Route exact path='/' component={ Landing } />
             <Route path='/contact' component={ Contact }/>
             <Route path='/projects' component={ Projects }/>
         </div>
-    </HashRouter>
+    </BrowserRouter>
 )
